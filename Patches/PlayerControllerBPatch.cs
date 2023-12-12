da9ca72054c395e69e316adb9719e9bab4d56d7e
Chrigi2486 <christian@cataldo.co.uk>
2023-12-12 17:51:09 +0100

    SUm Tings

---
+++ Patches/PlayerControllerBPatch.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GameNetcodeStuff;
+using HarmonyLib;
+using UnityEngine;
+
+namespace LCTestModChrigi.Patches
+{
+
+    
+    internal class ChrigiGamePatch
+    {
+
+        private static bool ded = false;
+        private static RoundManager currentRound;
+
+
+        [HarmonyPatch(typeof(RoundManager), "LoadNewLevel")]
+        [HarmonyPrefix]
+        static void LoadNewLevelPatch()
+        {
+            Debug.Log("SNATCHED ROUNDMANAGER");
+            currentRound = RoundManager.Instance;
+        }
+
+        [HarmonyPatch(typeof(PlayerControllerB))]
+        [HarmonyPatch("Update")]
+        [HarmonyPostfix]
+        static void infiniteSprintPatch(ref float ___sprintMeter, ref float ___sprintMultiplier, ref float ___drunkness, ref bool ___isSprinting, ref Vector3 ___oldPlayerPosition)
+        {
+            //___sprintMeter = 1f;
+
+            if (___isSprinting)
+            {
+                ___sprintMultiplier = 10f;
+                //Landmine.SpawnExplosion(___oldPlayerPosition, true);
+            }
+            // ___drunkness = 10f;
+            ded = false;
+        }
+
+
+        [HarmonyPatch(typeof(PlayerControllerB))]
+        [HarmonyPatch("KillPlayer")]
+        [HarmonyPostfix]
+        static void patchDeath(ref Vector3 ___placeOfDeath)
+        {
+
+            if (!ded)
+            {
+                ded = true;
+                Debug.Log("SPAWN CAUSE DED");
+                currentRound.SpawnEnemyOnServer(___placeOfDeath, 0f, 2);
+            }
+        }
+        
+        
+    }
+
+
+}
---
+++ Plugin.cs
@@ -0,0 +1,51 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+using HarmonyLib;
+using LCTestModChrigi.Patches;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LCTestModChrigi
+{
+    [BepInPlugin(modGUID, modName, modVersion)]
+    public class TestModBase : BaseUnityPlugin
+    {
+        private const string modGUID = "Chrigi.LCTestMod";
+        private const string modName = "LC Test Mod";
+        private const string modVersion = "1.0.0";
+
+        private readonly Harmony harmony = new Harmony(modGUID);
+
+
+        private static TestModBase Instance;
+
+
+        internal ManualLogSource mls;
+
+
+        void Awake()
+        {
+            if (Instance == null) {
+                Instance = this;
+            }
+
+            mls = BepInEx.Logging.Logger.CreateLogSource(modGUID);
+
+
+            mls.LogInfo("Test mod has arisen!");
+
+
+            harmony.PatchAll(typeof(ChrigiGamePatch));
+
+
+
+
+        }
+
+
+
+    }
+}

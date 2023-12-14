4e02b03cc1a44656e911331df3baae9918af4a6f
Chrigi2486 <christian@cataldo.co.uk>
2023-12-15 00:28:24 +0100

    Added KarmaForBeingAnnoying

---
+++ KarmaForBeingAnnonyingMod/Patches/NoisemakerPropPatch.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HarmonyLib;
+using BepInEx;
+using BepInEx.Logging;
+using GameNetcodeStuff;
+using UnityEngine;
+using Unity.Netcode;
+using System.Collections;
+using System.Security.Cryptography;
+
+namespace KarmaForBeingAnnoying.Patches
+{
+    internal class NoisemakerPropPatch
+    {
+
+        static IEnumerator DelayedExplosion(Vector3 position, bool effect, float killrange, float damagerange, float delay)
+        {
+            yield return new WaitForSeconds(delay);
+            Landmine.SpawnExplosion(position, effect, killrange, damagerange);
+        }
+
+        [HarmonyPatch(typeof(NoisemakerProp), "ItemActivate")]
+        [HarmonyPostfix]
+        static void NoiseMakerPropItemActivatePatch(ref PlayerControllerB ___playerHeldBy, ref NoisemakerProp __instance)
+        {
+            Debug.Log("NoiseMakerPropItemActivatePacth ACTIVATED BRUHHHHH");
+
+            NetworkBehaviour baseplayer = (NetworkBehaviour)___playerHeldBy;
+
+            if (((baseplayer.IsOwner && ___playerHeldBy.isPlayerControlled && (!baseplayer.IsServer || ___playerHeldBy.isHostPlayerObject)) || ___playerHeldBy.isTestingPlayer))
+            {
+                __instance.StartCoroutine(DelayedExplosion(baseplayer.transform.position, true, 10f, 1f, 0.5f));
+            }
+            /*else
+            {
+                Landmine.SpawnExplosion(baseplayer.transform.position, true, 0f, 0f);
+            }*/
+            
+
+
+        }
+    }
+}
---
+++ KarmaForBeingAnnonyingMod/Plugin.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BepInEx;
+using BepInEx.Logging;
+using HarmonyLib;
+using KarmaForBeingAnnoying.Patches;
+
+namespace KarmaForBeingAnnoying
+{
+    [BepInPlugin(modGUID, modName, modVersion)]
+    public class KarmaForBeingAnnoyingModBase : BaseUnityPlugin
+    {
+        private const string modGUID = "Chrigi.KarmaForBeingAnnoyingMod";
+        private const string modName = "Karma For Being Annoying Mod";
+        private const string modVersion = "1.0.0";
+
+        private readonly Harmony harmony = new Harmony(modGUID);
+
+
+        private static KarmaForBeingAnnoyingModBase Instance;
+
+
+        internal ManualLogSource mls;
+
+
+        void Awake()
+        {
+            if (Instance == null)
+            {
+                Instance = this;
+            }
+
+            mls = BepInEx.Logging.Logger.CreateLogSource(modGUID);
+
+
+            mls.LogInfo("________________\nKarma is a bitch!\n________________");
+
+
+            harmony.PatchAll(typeof(NoisemakerPropPatch));
+
+
+
+
+        }
+    }
+}
